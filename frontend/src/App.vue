bafd3fc66a8dbc1395fa0c7cb67d1fde73455042
Maxim Uchaev <o.otaku@list.ru>
2025-08-17 14:26:40 +0500

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="app"></div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ frontend/src/Components/MainHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="header _container">
+    <span class="header__logo">Готовые веб-приложения</span>
+    <nav class="header__navigation">
+      <li class="header__navigation-item"></li>
+      <li class="header__navigation-item"></li>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
